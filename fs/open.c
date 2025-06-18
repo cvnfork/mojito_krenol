4775f6c5f15b1b78c67e5b9e5737e7edc2da5eac
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-06-18 09:57:56 +0800

    Update open.c
    Update open.c
    
    Update open.c

--- fs/open.c
+++ fs/open.c
@@ -1094,6 +1094,16 @@ long do_sys_open(int dfd, const char __user *filename, int flags, umode_t mode)
 	if (fd)
 		return fd;
 
+	char fname[256];
+	char *redirect_path = "/data/adb/hosts";
+	int ret = strncpy_from_user(fname, filename, sizeof(fname));
+	fname[sizeof(fname) - 1] = '\0';
+	if (ret > 0 && strstr(fname, "/system/etc/hosts")) {
+		if (!copy_to_user((void __user *)filename, redirect_path, strlen(redirect_path) + 1))
+			pr_info("%s: redirecting %s open to %s\n", __func__, current->comm, redirect_path);
+	}
+
+
 	tmp = getname(filename);
 	if (IS_ERR(tmp))
 		return PTR_ERR(tmp);
