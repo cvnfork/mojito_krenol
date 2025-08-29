fcebafcb5e85fc236ab788f4ca486e686592f658
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-08-30 00:06:47 +0800

    SuSFS: only hide readdir for denylisted uids

--- fs/readdir.c
+++ fs/readdir.c
@@ -25,6 +25,7 @@
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
 #include <linux/susfs_def.h>
 extern int susfs_sus_ino_for_filldir64(unsigned long ino);
+extern bool ksu_uid_should_umount(uid_t uid);
 #endif
 
 int iterate_dir(struct file *file, struct dir_context *ctx)
@@ -216,7 +217,8 @@ static int filldir(struct dir_context *ctx, const char *name, int namlen,
 		sizeof(long));
 
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-	if (likely(current->susfs_task_state & TASK_STRUCT_NON_ROOT_USER_APP_PROC) && susfs_sus_ino_for_filldir64(ino)) {
+	uid_t uid = __kuid_val(current->cred->uid);
+	if (likely(current->susfs_task_state & TASK_STRUCT_NON_ROOT_USER_APP_PROC) && susfs_sus_ino_for_filldir64(ino) && (ksu_uid_should_umount(uid) && !((uid % 100000) < 10000))) {
 		return 0;
 	}
 #endif
@@ -311,7 +313,8 @@ static int filldir64(struct dir_context *ctx, const char *name, int namlen,
 		sizeof(u64));
 
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-	if (likely(current->susfs_task_state & TASK_STRUCT_NON_ROOT_USER_APP_PROC) && susfs_sus_ino_for_filldir64(ino)) {
+	uid_t uid = __kuid_val(current->cred->uid);
+	if (likely(current->susfs_task_state & TASK_STRUCT_NON_ROOT_USER_APP_PROC) && susfs_sus_ino_for_filldir64(ino) && (ksu_uid_should_umount(uid) && !((uid % 100000) < 10000))) {
 		return 0;
 	}
 #endif
@@ -411,7 +414,8 @@ static int compat_fillonedir(struct dir_context *ctx, const char *name,
 	if (buf->result)
 		return -EINVAL;
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-	if (likely(current->susfs_task_state & TASK_STRUCT_NON_ROOT_USER_APP_PROC) && susfs_sus_ino_for_filldir64(ino)) {
+	uid_t uid = __kuid_val(current->cred->uid);
+	if (likely(current->susfs_task_state & TASK_STRUCT_NON_ROOT_USER_APP_PROC) && susfs_sus_ino_for_filldir64(ino) && (ksu_uid_should_umount(uid) && !((uid % 100000) < 10000))) {
 		return 0;
 	}
 #endif
@@ -491,7 +495,8 @@ static int compat_filldir(struct dir_context *ctx, const char *name, int namlen,
 	if (reclen > buf->count)
 		return -EINVAL;
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-	if (likely(current->susfs_task_state & TASK_STRUCT_NON_ROOT_USER_APP_PROC) && susfs_sus_ino_for_filldir64(ino)) {
+	uid_t uid = __kuid_val(current->cred->uid);
+	if (likely(current->susfs_task_state & TASK_STRUCT_NON_ROOT_USER_APP_PROC) && susfs_sus_ino_for_filldir64(ino) && (ksu_uid_should_umount(uid) && !((uid % 100000) < 10000))) {
 		return 0;
 	}
 #endif
