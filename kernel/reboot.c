7cbe903ea6ec259ccf42e192e3b2cf360704d4c7
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-01-06 00:19:05 +0800

    revert hooks

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -278,10 +278,6 @@ static DEFINE_MUTEX(reboot_mutex);
  * reboot doesn't sync: do that yourself before calling this.
  */
 
-#ifdef CONFIG_KSU
-extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);
-#endif
-
 #ifdef CONFIG_MAP_SPOOF
 extern int lkm_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);
 #endif
@@ -292,10 +288,6 @@ SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 	struct pid_namespace *pid_ns = task_active_pid_ns(current);
 	char buffer[256];
 	int ret = 0;
-
-#ifdef CONFIG_KSU 
-	ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
-#endif
 
 #ifdef CONFIG_MAP_SPOOF
 	lkm_handle_sys_reboot(magic1, magic2, cmd, &arg);
