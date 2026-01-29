1174784e58882a4de354ea6d7e45529072adedf5
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-01-29 16:40:21 +0800

    fixup! BACKPORT: mm: introduce wrappers to access mm->nr_ptes
    fixup 4b9dd7cd1487
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- mm/khugepaged.c
+++ mm/khugepaged.c
@@ -1332,7 +1332,6 @@ static void retract_page_tables(struct address_space *mapping, pgoff_t pgoff)
 				_pmd = pmdp_collapse_flush(vma, addr, pmd);
 				spin_unlock(ptl);
 				mm_dec_nr_ptes(vma->vm_mm);
-				atomic_long_dec(&mm->nr_ptes);
 				tlb_remove_table_sync_one();
 				vm_write_end(vma);
 				pte_free(mm, pmd_pgtable(_pmd));
